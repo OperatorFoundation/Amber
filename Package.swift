2f9a3d863522101712b9c92b94f0163e9db16778
bragelbytes <joebragel@gmail.com>
2023-10-02 18:41:29 -0700

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -26,7 +26,7 @@ let package = Package(
         .package(url: "https://github.com/OperatorFoundation/Datable", from: "4.0.0"),
         .package(url: "https://github.com/OperatorFoundation/Gardener", branch: "release"),
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
-        .package(url: "https://github.com/OperatorFoundation/swift-ast", branch: "release"),
+        .package(url: "https://github.com/OperatorFoundation/swift-ast", from: "0.19.12"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
